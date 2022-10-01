f94f33b94acf2344531a7de5f90a3b955c3bc8bd
amolgarad96 <amolgarad1748@gmail.com>
2022-10-01 20:06:32 +0530

    finally sanjay placed

---
+++ src/code/EX1.java
@@ -0,0 +1,12 @@
+package code;
+
+public class EX1 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("hii sanjay,how are you");
+		System.out.println("sanjay got 10LPA in amdocs");
+		
+	}
+}
